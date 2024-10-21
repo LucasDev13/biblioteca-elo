58c88bd149d1eaf6f6346ee74d9c9f07bdbd6082
Lucas Pontes <lucaspontes140@gmail.com>
2024-10-21 00:55:52 -0300

    dev: Refatorado os campos obrigatórios dos request.

--- src/main/java/br/com/elotech/biblioteca_elo/interfacesAdapters/controllers/request/UserRequest.java
+++ src/main/java/br/com/elotech/biblioteca_elo/interfacesAdapters/controllers/request/UserRequest.java
@@ -1,22 +1,18 @@
 package br.com.elotech.biblioteca_elo.interfacesAdapters.controllers.request;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 
 public record UserRequest(
-        @NotBlank(message = "Não pode ser em branco e null!")
-        @NotEmpty(message = "Não pode estar vazio!")
+        @NotEmpty(message = "Não pode estar vazio ou null!")
         String name,
 
-        @NotBlank(message = "Não pode ser em branco e null!")
-        @NotEmpty(message = "Não pode estar vazio!")
+        @NotEmpty(message = "Não pode estar vazio ou null!")
         @Email
         String email,
 
-        @NotBlank(message = "Não pode ser em branco e null!")
-        @NotEmpty(message = "Não pode estar vazio!")
+        @NotEmpty(message = "Não pode estar vazio ou null!")
         @Pattern(regexp = "\\d{12}", message = "O campo deve conter exatamente 12 dígitos")
         String phoneNumber) {
 }
