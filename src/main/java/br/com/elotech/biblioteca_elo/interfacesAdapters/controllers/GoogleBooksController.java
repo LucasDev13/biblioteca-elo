8c780cae8aafd469a3ea6552682a34183a51e543
Lucas Pontes <lucaspontes140@gmail.com>
2024-11-08 14:18:43 -0300

    dev: Refatorado as classes GoogleBooksController e BookRecommendationController de record para class.

--- src/main/java/br/com/elotech/biblioteca_elo/interfacesAdapters/controllers/GoogleBooksController.java
+++ src/main/java/br/com/elotech/biblioteca_elo/interfacesAdapters/controllers/GoogleBooksController.java
@@ -4,8 +4,11 @@ import br.com.elotech.biblioteca_elo.application.usecases.GoogleBooksUseCase;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public record GoogleBooksController(
-        GoogleBooksUseCase googleBooksUseCase
-) {
+public class GoogleBooksController{
 
+    private final GoogleBooksUseCase googleBooksUseCase;
+
+    public GoogleBooksController(GoogleBooksUseCase googleBooksUseCase) {
+        this.googleBooksUseCase = googleBooksUseCase;
+    }
 }
