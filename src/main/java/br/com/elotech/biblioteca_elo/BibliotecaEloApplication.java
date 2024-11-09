3bc5a15d4b54d6a11ecbf9e4f00145b23edf0beb
Lucas Pontes <lucaspontes140@gmail.com>
2024-11-09 05:40:54 -0300

    dev: Refatorado classes que não estavam sendo injetadas pelo spring.

--- src/main/java/br/com/elotech/biblioteca_elo/BibliotecaEloApplication.java
+++ src/main/java/br/com/elotech/biblioteca_elo/BibliotecaEloApplication.java
@@ -4,9 +4,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "API de Livros da elotech", version = "1.0", description = "Documentação da API de Livros."))
+@EnableFeignClients(basePackages = "br.com.elotech.biblioteca_elo.infrastructure.middleware.gateways")
 public class BibliotecaEloApplication {
 
 	public static void main(String[] args) {
